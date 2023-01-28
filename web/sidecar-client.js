20188184dcbbb67bc426a29aa7beb08b974c0d9c
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-01-28 14:48:27 +0100

    add PDUs in the web page

--- web/sidecar-client.js
+++ web/sidecar-client.js
@@ -1,11 +1,11 @@
 // a sidecar client for the new websockets implementation
-"use strict";
+"use strict"
 
-let devel_url = "ws://localhost:10000/";
-let prod_url = "wss://r2lab.inria.fr:999/";
-let default_url = devel_url;
+let devel_url = "ws://localhost:10000/"
+let prod_url = "wss://r2lab.inria.fr:999/"
+let default_url = devel_url
 
-let websocket = undefined;
+let websocket = undefined
 
 ////////////////////
 let categories = {
@@ -17,73 +17,71 @@ let categories = {
              def_info: '[{"id":1, "airplane_mode":"on"}]',
              prettifier: pretty_records,
             },
+    pdus: {depth: 5, def_request: '"PLEASE"',
+            def_info: '[{"id": "jaguar", "on_off": "on"}]',
+            prettifier: pretty_records,
+            },
     leases: {depth: 2,  def_request: '"PLEASE"',
              def_info: '-- not recommended --',
              prettifier: pretty_leases,
             },
 }
 
-let actions = {
-    request: {color: "red", button: "Request update"},
-    info : {color: "green", button: "Send (json) data"},
-}
+//let actions = {
+//    request: {color: "red", button: "Request update"},
+//    info : {color: "green", button: "Send (json) data"},
+//}
 
 
 let populate = function() {
     $("input#url")
         .keypress(function(ev) {
-            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            var keycode = (ev.keyCode ? ev.keyCode : ev.which)
             if (keycode == '13')
-                set_url();
-        });
-    let areas = ['button', 'input'];
-    let anchor = $("#separator-controls");
+                set_url()
+        })
+    let anchor = $("#separator-controls")
+    function add_element(newdiv) {
+        anchor.after(newdiv)
+        anchor = newdiv
+        return newdiv
+    }
     for (let category in categories) {
         // insert in the right order, for tabindexes to work as expected
-        let newdiv =
-            $(`<div>`,
-                {id: `${category}-label`,
-                 style: `grid-area:${category}-label`})
-            .html(`${category}`);
-        anchor.after(newdiv);
-        anchor = newdiv;
-        // the 4 grid items for input / button and info / request
-        for (let action in actions) {
-            for (let area of areas) {
-                newdiv =
-                    $("<div>",
-                        {id: `${category}-${action}-${area}`,
-                         style: `grid-area:${category}-${action}-${area}`});
-                anchor.after(newdiv);
-                anchor = newdiv;
-            }
-            // create button
-            $(`#${category}-${action}-button`).append(
-                $(`<button>${actions[action].button}</button>`)
-                    .addClass(`${actions[action].color}`));
-            // set its behaviour
-            $(`div#${category}-${action}-button>button`).click(function(e){
-                send(category, action);
-            });
-        }
-        $(`#${category}-request-input`).append(
-            $(`<input />`)
-            .val(`${categories[category].def_request}`)
-            .keypress(function (ev) {
-                var keycode = (ev.keyCode ? ev.keyCode : ev.which);
-                if (keycode == '13')
-                    send(category, 'request')
-            })
-        );
-        $(`#${category}-info-input`).append(
-            $(`<input />`)
+        add_element(
+            $(`<div>`, {id: `${category}-label`, style: `grid-area:${category}-label`})
+            .html(`${category}`))
+
+        // request button - send button - input area
+        let buttons = add_element(
+            $(`<div>`, {id: `${category}-buttons`, style: `grid-area:${category}-buttons`})
+            .addClass("category-buttons"))
+        // create button
+        buttons.append(
+                $(`<button>`, {id: `${category}-request`})
+                .html("request update")
+                .addClass(`red`))
+        // set its behaviour
+        $(`#${category}-request`).click((e) => send(category, "request"))
+
+        // create 2nd button
+        buttons.append(
+            $(`<button>/`, {id: `${category}-info`})
+            .html('send data')
+            .addClass(`green`))
+        // set its behaviour
+        $(`#${category}-info`).click((e) => send(category, 'info'))
+
+        buttons.append(
+            $(`<input />`, {id: `${category}-input`})
             .val(`${categories[category].def_info}`)
             .keypress(function (ev) {
-                var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+                let keycode = (ev.keyCode ? ev.keyCode : ev.which)
                 if (keycode == '13')
                     send(category, 'info')
             })
-        );
+        )
+
 
         // contents area
         $(`#top-contents`).append(
@@ -95,102 +93,102 @@ let populate = function() {
                 .append(
                     $(`<ul>`,
                       {id: `ul-${category}`, class: "contents"}))
-        );
-        $("button").attr('tabindex', -1);
+        )
+        $("button").attr('tabindex', -1)
     }
 }
 
 
 //////////////////// global functions
 function set_status(text, color) {
-    $("#connection-status").css("background-color", color).html(text);
+    $("#connection-status").css("background-color", color).html(text)
 }
 
 function update_status() {
-    let color;
-    let text;
+    let color
+    let text
     if (websocket === undefined) {
-        color = "gray";
-        text = "idle";
+        color = "gray"
+        text = "idle"
     } else switch (websocket.readyState) {
         case websocket.OPEN:
-            color = "green";
-            text = `connected to ${websocket.url}`;
-            break;
+            color = "green"
+            text = `connected to ${websocket.url}`
+            break
         case websocket.OPEN:
-            color = "green";
-            text = `connected to ${websocket.url}`;
-            break;
+            color = "green"
+            text = `connected to ${websocket.url}`
+            break
         case websocket.CONNECTING:
-            color = "orange";
-            text = `connecting to ${websocket.url}`;
-            break;
+            color = "orange"
+            text = `connecting to ${websocket.url}`
+            break
         default:
-            color = "red";
-            text = `connection closed to ${websocket.url}`;
+            color = "red"
+            text = `connection closed to ${websocket.url}`
     }
-    set_status(text, color);
+    set_status(text, color)
 }
 
 
 function cyclic_update() {
     setTimeout(function () {
-        update_status();
-        cyclic_update();
-    }, 1000);
+        update_status()
+        cyclic_update()
+    }, 1000)
 }
 
-cyclic_update();
+cyclic_update()
 
 
 
 function show_connected(url) {
-    $("#connection-status").css("background-color", "green");
-    $("#connection-status").html("connected to " + url);
+    $("#connection-status").css("background-color", "green")
+    $("#connection-status").html("connected to " + url)
 }
 function show_disconnected(message) {
-    $("#connection-status").css("background-color", "gray");
-    $("#connection-status").html(message);
+    $("#connection-status").css("background-color", "gray")
+    $("#connection-status").html(message)
 }
 function show_failed_connection(url) {
-    $("#connection-status").css("background-color", "red");
-    $("#connection-status").html("connection failed to " + url);
+    $("#connection-status").css("background-color", "red")
+    $("#connection-status").html("connection failed to " + url)
 }
 
 function connect_sidecar(url) {
     if (websocket) {
-        disconnect();
+        disconnect()
     }
-    console.log(`Connecting to sidecar at ${url}`);
-    websocket = new WebSocket(url);
-    set_status("connecting ..", "orange");
+    console.log(`Connecting to sidecar at ${url}`)
+    websocket = new WebSocket(url)
+    set_status("connecting ..", "orange")
 
     // ignore events that do not target the current websocket instance
     // this may happen typically when reconnecting, we close and immediately
     // re-open, but the close event comes back referring to the previous connection
     websocket.onopen = function(event) {
         if (event.target != websocket)
-            return;
-        update_status();
+            return
+        update_status()
     }
     websocket.onclose = function(event) {
         if (event.target != websocket)
-            return;
-        websocket = undefined;
-        update_status();
+            return
+        websocket = undefined
+        update_status()
     }
 
     websocket.onmessage = function(event) {
         if (event.target != websocket)
-            return;
-        console.log("receiving message on websocket", websocket);
-        let umbrella = JSON.parse(event.data);
+            return
+        console.log("receiving message on websocket", websocket)
+        let umbrella = JSON.parse(event.data)
         console.log(`websockets: incoming umbrella`, umbrella)
-        let category = umbrella.category;
-        let action = umbrella.action;
-        let message = umbrella.message;
+        let category = umbrella.category
+        let action = umbrella.action
+        let message = umbrella.message
         if (action == "info") {
-            update_contents(category, message);
+            update_contents(category, message)
         }
     }
 }
@@ -198,60 +196,60 @@ function connect_sidecar(url) {
 
 ////////////////////
 let set_url = function(e) {
-    let url = $('input#url').val();
+    let url = $('input#url').val()
     if (url == "") {
-        url = default_url;
-        $('input#url').val(url);
+        url = default_url
+        $('input#url').val(url)
     }
-    connect_sidecar(url);
+    connect_sidecar(url)
 }
 
 let disconnect = function() {
     if (websocket == undefined) {
-        console.log("already disconnected");
-        return;
+        console.log("already disconnected")
+        return
     }
     console.log("disconnecting")
-    websocket.close();
-    websocket = undefined;
-    show_disconnected("idle");
+    websocket.close()
+    websocket = undefined
+    show_disconnected("idle")
 }
 
 let set_devel_url = function(e) {
-    $('input#url').val(devel_url);
-    disconnect();
-    set_url();
+    $('input#url').val(devel_url)
+    disconnect()
+    set_url()
 }
 
 let set_prod_url = function(e) {
-    $('input#url').val(prod_url);
-    disconnect();
-    set_url();
+    $('input#url').val(prod_url)
+    disconnect()
+    set_url()
 }
 //////////////////// the 3 channels
 let pad2 = (number) => number.toString().padStart(2, 0)
 function prettyDate() {
-    let now = new Date();
+    let now = new Date()
     return `${pad2(now.getHours())}:`
     + `${pad2(now.getMinutes())}:`
-    + `${pad2(now.getSeconds())}`;
+    + `${pad2(now.getSeconds())}`
 }
 
 // a function to prettify the leases message
 function pretty_leases(records) {
-    records.sort((r1, r2) => r1.valid_from.localeCompare(r2.valid_from));
-    let bullets = $(`<div>`).addClass('records');
-    console.log(records);
+    records.sort((r1, r2) => r1.valid_from.localeCompare(r2.valid_from))
+    let bullets = $(`<div>`).addClass('records')
+    console.log(records)
     records.forEach(function(lease) {
         try {
-            let [datef, timef] = lease.valid_from.split("T");
-            let [dateu, timeu] = lease.valid_until.split("T");
+            let [datef, timef] = lease.valid_from.split("T")
+            let [dateu, timeu] = lease.valid_until.split("T")
             let inside = $(`<ul>`)
                 .append($("<li>").html(`From ${datef}`))
                 .append($("<li>").html(`at ${timef}`))
                 .append($("<li>").html(`Until ${dateu}`))
                 .append($("<li>").html(`at ${timeu}`))
-            ;
+
             bullets.append(
                 $(`<span>`)
                 .addClass('record')
@@ -266,74 +264,80 @@ function pretty_leases(records) {
             )
         }
     })
-    return bullets;
+    return bullets
 }
 
 // applicable to nodes and phones
 function pretty_records(records) {
-    records.sort((r1, r2) => r1.id - r2.id);
+    records.sort((r1, r2) => r1.id - r2.id)
     let bullets = $(`<div>`)
-        .addClass('records');
+        .addClass('records')
     records.forEach(function(record) {
-        let inside = $(`<ul>`);
+        let inside = $(`<ul>`)
         for (let attribute of Object.keys(record).sort())
-            inside.append($(`<li>`).html(`"${attribute}" : ${JSON.stringify(record[attribute])}`));
+            inside.append($(`<li>`).html(`"${attribute}" : ${JSON.stringify(record[attribute])}`))
         let color_class =
             ((record['available'] == 'ko')
-             || (record['airplane_mode'] == 'fail')) ? "red" : "";
+             || (record['airplane_mode'] == 'fail')) ? "red" : ""
         bullets.append(
             $(`<span>`)
                 .addClass('record')
                 .addClass(color_class)
                 .html(record.id)
                 .tooltip({title: inside, html: true}))
-    });
-    return bullets;
+    })
+    return bullets
 }
 
 // update the 'contents' <ul> and keep at most <depth> entries in there
 function update_contents(name, value) {
-    let ul_sel = `#ul-${name}`;
-    let $ul = $(ul_sel);
-    let prettifier = categories[name].prettifier;
-    let pretty = prettifier(value);
+    let ul_sel = `#ul-${name}`
+    let $ul = $(ul_sel)
+    let prettifier = categories[name].prettifier
+    let pretty = prettifier(value)
     let item = $(`<li>`)
         .append($(`<span class="date">${prettyDate()}</span>`)
-                .append(pretty));
+                .append(pretty))
 
-    let depth = categories[name].depth;
-    let lis = $(`${ul_sel}>li`);
-    console.log(`trimming to ${depth} from ${lis.length}`);
+    let depth = categories[name].depth
+    let lis = $(`${ul_sel}>li`)
+    console.log(`trimming to ${depth} from ${lis.length}`)
     if (lis.length >= depth) {
         lis.last().remove()
     }
-    $(ul_sel).prepend(item);
+    $(ul_sel).prepend(item)
 }
 
 let clear_all = function() {
     for (let name in categories) {
-        $(`#ul-${name}`).html("");
+        $(`#ul-${name}`).html("")
     }
 }
 
 
 function send(category, action) {
-    let umbrella = {category: category, action: action};
-    let selector = `div#${category}-${action}-input>input`;
-    let strmessage = $(selector).val();
-    let objmessage = JSON.parse(strmessage);
-    umbrella.message = objmessage;
-    console.log(`emitting on category ${category} action ${action}`, umbrella);
-    let json = JSON.stringify(umbrella);
-    websocket.send(json);
-    return false;
+    let umbrella = {category: category, action: action}
+    let strmessage
+    if (action == "request")
+        strmessage = '"PLEASE"'
+    else {
+        let selector = `#${category}-input`
+        strmessage = $(selector).val()
+    }
+    console.log(`send strmessage=${strmessage}`)
+    let objmessage = JSON.parse(strmessage)
+    umbrella.message = objmessage
+    console.log(`emitting on category ${category} action ${action}`, umbrella)
+    let json = JSON.stringify(umbrella)
+    websocket.send(json)
+    return false
 }
 
 ////////////////////
 $(() => {
-    populate();
-    set_url();
-    let tooltips = $('[data-toggle="tooltip"]');
-    tooltips.tooltip();
+    populate()
+    set_url()
+    let tooltips = $('[data-toggle="tooltip"]')
+    tooltips.tooltip()
     set_prod_url()
 })
